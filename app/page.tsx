355985b880c550b0ced157cf5a3816d006854d70
Ludo Hund <Ludovic_hund@hotmail.fr>
2025-04-28 19:33:29 +0200

    add header and hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,54 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import HeroSection from "@/components/HeroSection/HeroSection";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <>
+      <Header />
+      <main className="mx-auto flex-1 overflow-hidden">
+        <HeroSection />
+      </main>
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+    </>
+  );
 }
---
+++ components/HeroSection/HeroSection.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ArrowRightIcon } from "lucide-react";
+import { Particles } from "../magicui/particles";
+import { Button } from "../ui/button";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function HeroSection() {
+  return (
+    <section className="flex items-center justify-center flex-col mt-32">
+      <Particles
+        className="absolute inset-0 z-0"
+        quantity={100}
+        ease={60}
+        refresh
+      />
+      <motion.h1
+        initial={{ opacity: 0, translateY: "-10px" }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="text-center bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl"
+      >
+        Développeur Fullstack
+        <br className="hidden md:block" /> Spécialisé Front-End
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0, translateY: "-10px" }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.2 }}
+        className="text-center mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance"
+      >
+        Un savoir-faire et une vision globale,
+        <br className="hidden md:block" /> de la conception à la mise en
+        production.
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, translateY: "-15px" }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.5 }}
+      >
+        <Link href={"#contact"}>
+          <Button className="gap-1 rounded-lg text-white dark:text-black cursor-pointer">
+            <span>Faisons connaissance </span>
+            <ArrowRightIcon className="ml-1 size-4" />
+          </Button>
+        </Link>
+      </motion.div>
+    </section>
+  );
+}
