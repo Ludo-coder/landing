7d85a8aeb2d33bbb62a3591bb6ae1a20d76d3feb
Ludo Hund <Ludovic_hund@hotmail.fr>
2025-04-29 10:37:51 +0200

    feat(client): section ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ClientSection from "@/components/ClientSection/ClientSection";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import HeroSection from "@/components/HeroSection/HeroSection";
@@ -6,8 +7,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="mx-auto flex-1 overflow-hidden">
+      <main className="mx-auto flex-1">
         <HeroSection />
+        <ClientSection />
       </main>
       <Footer />
       <Footer />
---
+++ components/ClientSection/ClientSection.tsx
@@ -0,0 +1,13 @@
+import { TextReveal } from "../magicui/text-reveal";
+
+export default function ClientSection() {
+  return (
+    <section>
+      <TextReveal>
+        Besoin d&apos;un site sur mesure, d&apos;une application performante ou
+        de conseils techniques ? Exprimez vos besoins, je m&apos;occupe du reste
+        !
+      </TextReveal>
+    </section>
+  );
+}
