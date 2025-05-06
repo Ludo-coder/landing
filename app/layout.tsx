e72679c0baf8a9b14cd7ee87e485bf5f5d4224eb
Ludo Hund <Ludovic_hund@hotmail.fr>
2025-05-06 14:40:04 +0200

    feat(config): seo, sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const geistSans = Geist({
 export const metadata: Metadata = {
   title: "Pixel by Ludo : Développeur Web",
   description:
-    "Un savoir-faire et une vision globale, de la conception à la mise en production. Développeur web Fullstack.",
+    "Développeur web Fullstack. Sites sur mesure, applications performantes, conseils techniques. Un savoir-faire et une vision globale, de la conception à la mise en production.",
 };
 
 export default function RootLayout({
@@ -25,6 +25,36 @@ export default function RootLayout({
       className="dark scroll-smooth"
       style={{ colorScheme: "dark" }}
     >
+      <head>
+        <meta name="robots" content="max-image-preview:large" />
+        <link rel="canonical" href="https://www.pixelbyludo.fr/" />
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:title"
+          content="Pixel by Ludo : Développeur Web"
+        ></meta>
+        <meta property="og:url" content="https://www.pixelbyludo.fr/"></meta>
+        <meta
+          property="og:site_name"
+          content="Pixel by Ludo : Développeur Web"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://www.pixelbyludo.fr/images/ludo.jpeg"
+        ></meta>
+        <meta property="og:image:width" content="3300"></meta>
+        <meta property="og:image:height" content="1698"></meta>
+        <meta property="og:image:alt" content=""></meta>
+        <meta property="og:locale" content="fr_FR"></meta>
+        <meta
+          name="application-name"
+          content="Pixel by Ludo : Développeur Web"
+        ></meta>
+        <meta
+          name="msapplication-window"
+          content="width=device-width;height=device-height"
+        ></meta>
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.pixelbyludo.fr/",
+  generateRobotsTxt: true,
+  priority: 1,
+};
